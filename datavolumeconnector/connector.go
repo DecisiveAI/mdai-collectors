a87616ff30e9af77fb9821c9c3a9691210033005
Robin Law <Robin-Law@users.noreply.github.com>
2025-01-10 17:17:36 -0600

    add traces/metrics counters, error handling

--- datavolumeconnector/connector.go
+++ datavolumeconnector/connector.go
@@ -20,8 +20,17 @@ type connectorImp struct {
 	component.ShutdownFunc
 }
 
+const (
+	dataTypeAttributeKey          = "data_type"
+	dataTypeLogsAttributeValue    = "logs"
+	dataTypeTracesAttributeValue  = "traces"
+	dataTypeMetricsAttributeValue = "metrics"
+)
+
 var (
-	plogMarshaler = plog.ProtoMarshaler{}
+	plogMarshaler    = plog.ProtoMarshaler{}
+	ptraceMarshaler  = ptrace.ProtoMarshaler{}
+	pmetricMarshaler = pmetric.ProtoMarshaler{}
 )
 
 func newConnector(logger *zap.Logger, config component.Config) (*connectorImp, error) {
@@ -38,7 +47,7 @@ func (c *connectorImp) Capabilities() consumer.Capabilities {
 }
 
 func (c *connectorImp) ConsumeLogs(ctx context.Context, logs plog.Logs) error {
-	metrics := pmetric.NewMetrics()
+	outputMetrics := pmetric.NewMetrics()
 
 	for i := 0; i < logs.ResourceLogs().Len(); i++ {
 		timestamp := pcommon.NewTimestampFromTime(time.Now())
@@ -49,7 +58,7 @@ func (c *connectorImp) ConsumeLogs(ctx context.Context, logs plog.Logs) error {
 
 		metricAttrMap := map[string]any{}
 
-		metricAttrMap["data_type"] = "logs"
+		metricAttrMap[dataTypeAttributeKey] = dataTypeLogsAttributeValue
 		for _, key := range c.config.LabelResourceAttributes {
 			if rawAttributes[key] != nil {
 				metricAttrMap[key] = rawAttributes[key]
@@ -58,8 +67,11 @@ func (c *connectorImp) ConsumeLogs(ctx context.Context, logs plog.Logs) error {
 			}
 		}
 
-		resourceMetrics := metrics.ResourceMetrics().AppendEmpty()
-		resourceMetrics.Resource().Attributes().FromRaw(metricAttrMap) // TODO: Handle error
+		resourceMetrics := outputMetrics.ResourceMetrics().AppendEmpty()
+		err := resourceMetrics.Resource().Attributes().FromRaw(metricAttrMap)
+		if err != nil {
+			c.logger.Error("error adding attributes to datavolume metric", zap.Error(err))
+		}
 		scopeMetric := resourceMetrics.ScopeMetrics().AppendEmpty()
 
 		if c.config.CountMetricName != "" {
@@ -69,6 +81,89 @@ func (c *connectorImp) ConsumeLogs(ctx context.Context, logs plog.Logs) error {
 
 		if c.config.BytesMetricName != "" {
 			bytes := int64(plogMarshaler.LogsSize(logs))
+			addMetric(scopeMetric, c.config.BytesMetricName, "bytes", timestamp, bytes)
+		}
+	}
+
+	return c.metricsConsumer.ConsumeMetrics(ctx, outputMetrics)
+}
+
+// ConsumeTraces method is called for each instance of a trace sent to the connector
+func (c *connectorImp) ConsumeTraces(ctx context.Context, traces ptrace.Traces) error {
+	outputMetrics := pmetric.NewMetrics()
+
+	for i := 0; i < traces.ResourceSpans().Len(); i++ {
+		timestamp := pcommon.NewTimestampFromTime(time.Now())
+		resourceSpans := traces.ResourceSpans().At(i)
+
+		resourceAttributes := resourceSpans.Resource().Attributes()
+		rawAttributes := resourceAttributes.AsRaw()
+
+		metricAttrMap := map[string]any{}
+
+		metricAttrMap[dataTypeAttributeKey] = dataTypeTracesAttributeValue
+		for _, key := range c.config.LabelResourceAttributes {
+			if rawAttributes[key] != nil {
+				metricAttrMap[key] = rawAttributes[key]
+			}
+		}
+
+		resourceMetrics := outputMetrics.ResourceMetrics().AppendEmpty()
+		err := resourceMetrics.Resource().Attributes().FromRaw(metricAttrMap)
+		if err != nil {
+			c.logger.Error("error adding attributes to datavolume metric", zap.Error(err))
+		}
+		scopeMetric := resourceMetrics.ScopeMetrics().AppendEmpty()
+
+		if c.config.CountMetricName != "" {
+			countValue := int64(resourceSpans.ScopeSpans().Len())
+			addMetric(scopeMetric, c.config.CountMetricName, "", timestamp, countValue)
+		}
+
+		if c.config.BytesMetricName != "" {
+			bytes := int64(ptraceMarshaler.TracesSize(traces))
+			addMetric(scopeMetric, c.config.BytesMetricName, "bytes", timestamp, bytes)
+		}
+	}
+
+	return c.metricsConsumer.ConsumeMetrics(ctx, outputMetrics)
+}
+
+func (c *connectorImp) ConsumeMetrics(ctx context.Context, metrics pmetric.Metrics) error {
+	outputMetrics := pmetric.NewMetrics()
+
+	for i := 0; i < metrics.ResourceMetrics().Len(); i++ {
+		timestamp := pcommon.NewTimestampFromTime(time.Now())
+		resourceLogs := metrics.ResourceMetrics().At(i)
+
+		resourceAttributes := resourceLogs.Resource().Attributes()
+		rawAttributes := resourceAttributes.AsRaw()
+
+		metricAttrMap := map[string]any{}
+
+		metricAttrMap[dataTypeAttributeKey] = dataTypeMetricsAttributeValue
+		for _, key := range c.config.LabelResourceAttributes {
+			if rawAttributes[key] != nil {
+				metricAttrMap[key] = rawAttributes[key]
+			} else {
+				metricAttrMap[key] = "unknown"
+			}
+		}
+
+		resourceMetrics := outputMetrics.ResourceMetrics().AppendEmpty()
+		err := resourceMetrics.Resource().Attributes().FromRaw(metricAttrMap)
+		if err != nil {
+			c.logger.Error("error adding attributes to datavolume metric", zap.Error(err))
+		}
+		scopeMetric := resourceMetrics.ScopeMetrics().AppendEmpty()
+
+		if c.config.CountMetricName != "" {
+			countValue := int64(resourceLogs.ScopeMetrics().Len())
+			addMetric(scopeMetric, c.config.CountMetricName, "", timestamp, countValue)
+		}
+
+		if c.config.BytesMetricName != "" {
+			bytes := int64(pmetricMarshaler.MetricsSize(metrics))
 			addMetric(scopeMetric, c.config.BytesMetricName, "bytes", timestamp, bytes)
 		}
 	}
@@ -90,15 +185,4 @@ func addMetric(scopeMetric pmetric.ScopeMetrics, metricName string, unit string,
 	dataPoint.SetTimestamp(timestamp)
 	dataPoint.SetStartTimestamp(timestamp)
 	dataPoint.SetIntValue(bytes)
-}
-
-// ConsumeTraces method is called for each instance of a trace sent to the connector
-func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) error {
-	//panic("ConsumeTraces is not implemented")
-	return nil
-}
-
-func (c *connectorImp) ConsumeMetrics(ctx context.Context, td pmetric.Metrics) error {
-	//panic("ConsumeMetrics is not implemented")
-	return nil
 }
